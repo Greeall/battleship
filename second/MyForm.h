cd8277c2b929571cee7192933a9ded21f947e433
Sereda Natalia <greeall@yandex.ru>
2017-04-08 23:10:19 +0300

    rr

--- second/MyForm.h
+++ second/MyForm.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <stdlib.h> 
+const int ARRANGE_SHIPS = 0;
+const int PLAYING = 1;
+const int EMPTY_CELL = 1;
+const int OCCUPIED_CELL = 0;
 
 namespace shipbattle {
 
@@ -43,13 +47,15 @@ namespace shipbattle {
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Button^  button2;
+
 	private: System::Windows::Forms::Label^  label2;
 	private: static array<System::Windows::Forms::Button^>^buttons;
 	private: array<int, 2>^ field; 
 	private: int current_height;
 	private: int limit_ships;
-	
+	private: int game_mode;
+	private: System::Windows::Forms::Button^  button2;
+
 
 
 
@@ -71,8 +77,8 @@ namespace shipbattle {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -101,16 +107,6 @@ namespace shipbattle {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(45, 479);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(100, 23);
-			this->button2->TabIndex = 4;
-			this->button2->Text = L"Save your ships";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
-			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
@@ -119,13 +115,23 @@ namespace shipbattle {
 			this->label2->Size = System::Drawing::Size(0, 13);
 			this->label2->TabIndex = 5;
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(289, 479);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Start game";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click_1);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(838, 514);
-			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox1);
@@ -137,9 +143,9 @@ namespace shipbattle {
 		}
 #pragma endregion
 
-private: void delete_buttons(int size_field)
+private: void delete_old_field(int size_field)
 {
-	for (int i = 0; i < size_field; i++) 
+	for (int i = 0; i < size_field*size_field; i++) 
 	{
 		this->Controls->Remove(this->buttons[i]); 
 	}
@@ -163,6 +169,11 @@ private: void generation_field(int height_field, System::Object^  sender, System
 		}
 }
 
+private: void arrange_ship(int i)
+{
+	buttons[i]->Text = "+";
+}
+
 private: int i_index(int index_click_button)
 {
 	return index_click_button/current_height;
@@ -175,33 +186,39 @@ private: int j_index(int index_click_button)
 
 private: void remove_access_to_area_about_ship(int i, int j)
 {
-	field[i,j] = 0;
+	field[i,j] = OCCUPIED_CELL;
+	
 	//up
 	if(i!=0)
-		field[i-1,j] = 0;
+		field[i-1,j] = OCCUPIED_CELL;
+		
 	//down
 	if(i!=current_height-1)
-		field[i+1,j] = 0;
+		field[i+1,j] = OCCUPIED_CELL;
+		
 	//left
 	if(j!=0)
-		field[i,j-1] = 0;
+		field[i,j-1] = OCCUPIED_CELL;
+		
 	//right
 	if(j!=current_height-1)
-		field[i,j+1] = 0;
+		field[i,j+1] = OCCUPIED_CELL;
 
 	//up left
 	if(i!=0 && j!=0)
-		field[i-1,j-1] = 0;
+		field[i-1,j-1] = OCCUPIED_CELL;
+
 	//up right
 	if(j!=current_height-1 && i!=0)
-		field[i-1,j+1] = 0;
+		field[i-1,j+1] = OCCUPIED_CELL;
 
 	//down left
 	if(i!=current_height-1 && j!=0)
-		field[i+1,j-1] = 0;
+		field[i+1,j-1] = OCCUPIED_CELL;
+
 	//dow right
 	if(i!=current_height-1 && j!=current_height-1)
-		field[i+1,j+1] = 0;
+		field[i+1,j+1] = OCCUPIED_CELL;
 
 }
 
@@ -210,63 +227,76 @@ private: void check_limit_ship()
 	int max_ships;
 	switch(current_height)
 	{
-	case 2: max_ships = 1; break;
-	case 3: max_ships = 2; break;
-	case 4: max_ships = 3; break;
-	case 5: max_ships = 5; break;
-	case 6: max_ships = 7; break;
-	case 7: max_ships = 8; break;
-	case 8: max_ships = 10; break;
-	case 9: max_ships = 12; break;
-	case 10: max_ships = 14; break;
+		case 2: max_ships = 1; break;
+		case 3: max_ships = 2; break;
+		case 4: max_ships = 3; break;
+		case 5: max_ships = 4; break;
+		case 6: max_ships = 6; break;
+		case 7: max_ships = 8; break;
+		case 8: max_ships = 10; break;
+		case 9: max_ships = 12; break;
+		case 10: max_ships = 15; break;
 	}
 
-	//if(limit_ships == max_ships) 
-		//save_ships_on_field();
+	if(limit_ships == max_ships) 
+		game_mode = PLAYING;
+		
+}
+
+private: bool cell_is_empty(int index_click_button)
+{
+	int i = i_index(index_click_button);
+	int j = j_index(index_click_button);
+	return (field[i, j] == EMPTY_CELL);
 }
 
 private: void try_select_ship(System::Object^ sender)
 {
-	//check_access_to_button();
-	//select_ship();
 	int index_click_button;
 	for(int i=0; i<current_height*current_height; i++)
 	if(buttons[i] == sender)
 		index_click_button = i;
-	buttons[index_click_button]->Text = "+";
-	limit_ships++;
-	remove_access_to_area_about_ship(i_index(index_click_button),j_index(index_click_button));
-	check_limit_ship();
+
+	if(game_mode == ARRANGE_SHIPS)
+	{
+		if(cell_is_empty(index_click_button))
+		{
+			arrange_ship(index_click_button);
+			limit_ships++;
+			remove_access_to_area_about_ship(i_index(index_click_button),j_index(index_click_button));
+			check_limit_ship();
+		}
+	}
+		
 }
 
-private: void make_free_field_in_array()
+private: void make_free_field()
 {
 	for(int i=0; i<current_height; i++)
 	{
 		for(int j=0; j<current_height; j++)
 		{
-			field[i,j] = 1;
+			field[i,j] = EMPTY_CELL;
 		}
 	}
 }
 
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-			 delete_buttons(current_height*current_height);
+			 delete_old_field(current_height);
 			 current_height = Convert::ToInt32(textBox1->Text);
 			 if(current_height > 10)
 				 current_height = 10;
 			 generation_field(current_height,sender,e);
-			 make_free_field_in_array();
-
-			
+			 make_free_field();
+			 game_mode = ARRANGE_SHIPS;
+			 limit_ships = 0;
 }
 
-private: System::Void buttons_Click(System::Object^  sender, System::EventArgs^  e) {
-			
-			 try_select_ship(sender);
-			// label2->Text = Convert::ToString(buttons[index_click_button]->Name);
-			 
-		 }
+private: System::Void buttons_Click(System::Object^  sender, System::EventArgs^  e)
+{
+	if(game_mode == ARRANGE_SHIPS)
+		try_select_ship(sender);
+}
 
  private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
 {
@@ -274,5 +304,7 @@ private: System::Void buttons_Click(System::Object^  sender, System::EventArgs^ 
 	 
 }
 
+private: System::Void button2_Click_1(System::Object^  sender, System::EventArgs^  e) {
+		 }
 };
 }
